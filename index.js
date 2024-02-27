1b5603a963377c28f254ad89cd2f742a9dc6d95b
Shivam <96533413+ShivamChavan01@users.noreply.github.com>
2024-02-27 12:35:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+// const dice = document.getElementById("dice");
+// const label= document.getElementById("mylabel");
+// let randomNumber;
+
+// let min=100;
+// max=1000000;
+
+// dice.onclick = function(){
+//     randomNumber=Math.floor(Math.random()*(max-min))+min;
+
+//     label.textContent=randomNumber;
+
+// }
+
+
+// let age=10;
+// let message = age>5 ? "you are a child":"you are not an adult";
+// console.log(message);
+
+// let username = window.prompt("Enter Your Username")
+// console.log(username);
+
+// username = username.trim().charAt(0).toUpperCase() + username.trim().slice(1).toLowerCase();
+// console.log(username);
+
+let fruits = ["apple", "banana", "oranges"];
+let vegetables = ["carrots","potatoes","raddish"]
+
+let foods = [...fruits,"eggs"];
+console.log(foods);
+
+
